ac3183e46fdf1c098db9a59270d352c374b82f97
Anton Lastukhin <lastuhinanton74@gmail.com>
2023-02-06 18:03:14 +0300

    add 04 part of 00 day

---
+++ 00_day/day00_ex04.sql
@@ -0,0 +1,13 @@
+SELECT
+       CONCAT (
+        name,
+        ' (age:',
+        age,
+        ',gender:''',
+        gender,
+        ''',address:''',
+        address,
+        ''')'
+        ) AS person_information
+FROM person
+ORDER BY age ASC
