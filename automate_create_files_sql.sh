37903916b857a1f304fc2e8ec289a64ff8f604e9
Anton Lastukhin <lastuhinanton74@gmail.com>
2023-02-08 21:49:41 +0300

    update script

--- automate_create_files_sql.sh
+++ automate_create_files_sql.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# for i in $(seq 0 $2); do
+#     file="day"
+#     if [[ $i -le 9 ]]; then
+#         file="${file}$1_ex0${i}.sql"
+#     else
+#         file="${file}$1_ex${i}.sql"
+#     fi
+#     touch $file
+# done
+
 for i in $(seq 0 $2); do
     file="day"
     if [[ $i -le 9 ]]; then
+        directory="ex0${i}"
         file="${file}$1_ex0${i}.sql"
     else
+        directory="ex${i}"
         file="${file}$1_ex${i}.sql"
     fi
-    touch $file
+    mkdir $directory
+    mv $file ./$directory/
 done
